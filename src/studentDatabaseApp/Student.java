ff5d78729c6ca9943ed071effbb776a0904f60c8
Ashwani <ashwanitrivedi12@gmail.com>
2020-09-30 16:36:39 +0530

    updated array of objects logic

--- src/studentDatabaseApp/Student.java
+++ src/studentDatabaseApp/Student.java
@@ -10,7 +10,7 @@ public class Student {
 	private String studentID;
 	private String courses = null;
 	private String course;
-	private int tuitionBalance;
+	private int tuitionBalance = 0;
 	private int costOfCourse = 600;
 	private static int Id = 1000;
 	
@@ -30,7 +30,7 @@ public class Student {
 		
 		setStudentId();
 		
-		System.out.println(firstName + " " + lastName + " " +gradeYear+ " "+ studentID);
+		//System.out.println(firstName + " " + lastName + " " +gradeYear+ " "+ studentID);
 		 	
 	}
 	
@@ -50,18 +50,43 @@ public class Student {
 			course = sc.nextLine();
 			if(!course.equals("Q")){
 				courses = courses + course + "\n" ;
+				tuitionBalance = tuitionBalance + costOfCourse;
 			}
 			else{ break; }
 		}while(1 != 0);
-		System.out.println("Courses enrolled :\n" + courses);
+		//System.out.print("Courses enrolled :"+ "\n" + courses);
 	}
 	
 	
 	
 	//view balance
+	public void viewBalance(){
+		System.out.println("Your tuition balance is:" + tuitionBalance);
+	}
+	
 	
 	//pay tuition
+	public void payTuition(){
+		viewBalance();
+		System.out.print("Enter your payment : Rs");
+		Scanner sc =  new Scanner(System.in);
+		int payment = sc.nextInt();
+		tuitionBalance = tuitionBalance - payment;
+		System.out.println("Thank you for payment of Rs." + payment);
+		viewBalance();
+		
+		
+	}
 	
 	//show status
+	public String showStatus(){
+		return "Name :" + firstName + " " + lastName +
+				"\nGradeYear :" + gradeYear +
+				"\nStudentId :" + studentID +
+				"\nCourses Enrolled : " + courses ;
+			
+	}
+	
+	
 	
 }
--- src/studentDatabaseApp/StudentDatabaseApp.java
+++ src/studentDatabaseApp/StudentDatabaseApp.java
@@ -1,14 +1,38 @@
 package studentDatabaseApp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
 		
-		Student s1 = new Student();
-		s1.enroll();
+		//Student s1 = new Student();
+		//s1.enroll();
+		//s1.viewBalance();
+		//s1.payTuition();
+		//System.out.println(s1.showStatus());
+		
 		//  how many new users you want to add
+		System.out.print("Enter number of students :");
+		Scanner sc = new Scanner(System.in);
+		int numOfStudents = sc.nextInt();
 		
 		//create n number of new students
+		Student[] students = new Student[numOfStudents];
+		for(int i=0; i<numOfStudents; i++)
+		  {
+			students[i] = new Student();
+			students[i].enroll();
+			students[i].payTuition();
+			
+		  }
+		
+		for(int i=0; i<numOfStudents; i++)
+		  {
+			System.out.print("Students enrolled are:\n" + students[i].showStatus());
+			
+		  }
+		
 
 	}
 
